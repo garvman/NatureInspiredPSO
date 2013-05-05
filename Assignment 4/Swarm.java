ae6d6b442ad51d1abec641ac92240611ba9f5901
Jim Garvey <bowdoinfb1@gmail.com>
2013-05-05 12:16:35 -0400

    reading in values from command line complete

--- Assignment 4/Swarm.java
+++ Assignment 4/Swarm.java
@@ -1,24 +1,28 @@
 
 import java.lang.Object;
-import java.util.Random;
+import java.util.*;
 
 public class Swarm {
 
+	
 	Particle [] swarm;
 	
-	public int NUM_DIMENSIONS;
-	public int NUM_PARTICLES;
-	public int NUM_ITERATIONS;
-	public int FUNCTION_NUMBER;
-	public int NT_NUMBER;
-	public int NIS_NUMBER;
-	public boolean PARTICLE_INCLUDED;
+	public static int NUM_DIMENSIONS;
+	public static int NUM_PARTICLES;
+	public static int NUM_ITERATIONS;
+	public static int FUNCTION_NUMBER;
+	public static int NT_NUMBER;
+	public static int NIS_NUMBER;
+	public static boolean PARTICLE_INCLUDED;
 	
 	
 	public static void main(String[] args){
+	
+		checkArgs(args);
+	
 		Swarm swarm = new Swarm ();
 		swarm.initSwarm();
-		swarm.runSwarm();
+		//swarm.runSwarm();
 
 
 	}
@@ -61,6 +65,113 @@ public class Swarm {
 			}
 		}
 	
+	
+	}
+	
+	private static void checkArgs(String [] args){
+		System.out.println();
+		System.out.println();
+		
+		if(args.length != 7){
+			System.out.println("" + args.length);
+			printCorrectParamImplementation();
+			System.exit(1);
+	
+		}
+		else{
+		
+			FUNCTION_NUMBER = Integer.parseInt(args[0]);
+			NUM_DIMENSIONS = Integer.parseInt(args[1]); 
+			NUM_PARTICLES = Integer.parseInt(args[2]);
+			NUM_ITERATIONS = Integer.parseInt(args[3]);
+			NT_NUMBER = Integer.parseInt(args[4]);
+			NIS_NUMBER = Integer.parseInt(args[5]);
+			int pInc = Integer.parseInt(args[6]);
+			
+			if(pInc == 1){
+				PARTICLE_INCLUDED = true;
+			}
+			else{
+				PARTICLE_INCLUDED = false;
+			}
+			
+			int i = 0;
+			if((FUNCTION_NUMBER > 4)||(FUNCTION_NUMBER < 0)){
+				System.out.println("Function Number out of bounds");
+				i++;
+			}
+			if(NUM_DIMENSIONS <= 0){
+				System.out.println("Number of Dimensions must be greater than 0");
+				i++;
+			}
+			if(NUM_PARTICLES <= 0){
+				System.out.println("Number of Particles must be greater than 0");
+				i++;
+			}
+			if(NUM_ITERATIONS <= 0){
+				System.out.println("Number of Iterations must be greater than 0");
+				i++;
+			}
+			if((NT_NUMBER > 3)||(NT_NUMBER < 0)){
+				System.out.println("Neighborhood Topology number is out of bounds");
+				i++;
+			}
+			if((NIS_NUMBER > 3)||(NIS_NUMBER < 0)){
+				System.out.println("Neighborhood Influence number is out of bounds");
+				i++;
+			}
+			if((pInc > 1)||(pInc < 0)){
+				System.out.println("Particle Included number must be either 1 or 0");
+				i++;
+			}
+			
+			if(i>0){
+				System.out.println();
+				System.out.println();
+				printCorrectParamImplementation();
+				System.exit(1);
+			}
+		
+		
+		}
+	
+	
+	}
+	private static void printCorrectParamImplementation(){
+	
+		System.out.println("Your input is invalid. It must have the following form:");
+		System.out.println();
+		System.out.println("java Swarm 'Fn Number' 'N Dimensions' 'N Particles' 'N Iterations'");
+		System.out.println("'Neighborhood Topology Num' 'Neighborhood Influence Num' 'Particle Included'");
+		System.out.println();
+		System.out.println("Where you should input the following numbers for desired parameters:");
+		System.out.println();
+		System.out.println("Function Number:");
+		System.out.println("SPHERE_FUNCTION = 0");
+		System.out.println("ROSENBROCK_FUNCTION = 1");
+		System.out.println("GRIEWANK_FUNCTION = 2");
+		System.out.println("ACKLEY_FUNCTION = 3");
+		System.out.println("RASTRIGIN_FUNCTION = 4");
+		System.out.println();
+		System.out.println("N Dimensions: desired number of dimensions for your run");
+		System.out.println("N Particles:  desired number of particles for your run");
+		System.out.println("N Iterations: desired number of iterations for your run");
+		System.out.println();
+		System.out.println("Neighborhood Topology Num:");
+		System.out.println("G_BEST = 0");
+		System.out.println("L_BEST = 1");
+		System.out.println("VON_NEUMANN = 2");
+		System.out.println("RANDOM_NEIGHBORHOOD = 3");
+		System.out.println();
+		System.out.println("Neighborhood Influence Num:");
+		System.out.println("FIPS = 0");
+		System.out.println("STD = 1");
+		System.out.println();
+		System.out.println("Particle Included:");
+		System.out.println("FALSE = 0");
+		System.out.println("TRUE = 1");
+		System.out.println();
+		System.out.println("Please re-enter your desired parameters");
 	
 	}
 
